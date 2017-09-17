ab33e73c2618110ade677d967527cb1d772dacf7
Kyle <kcdodaro@gmail.com>
2017-09-17 15:06:27 -0400

    Added axis information

--- DataAggregation/Form1.Designer.cs
+++ DataAggregation/Form1.Designer.cs
@@ -28,20 +28,265 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblStudent = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboGender = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblStudent
+            // 
+            this.lblStudent.AutoSize = true;
+            this.lblStudent.Location = new System.Drawing.Point(12, 9);
+            this.lblStudent.Name = "lblStudent";
+            this.lblStudent.Size = new System.Drawing.Size(50, 13);
+            this.lblStudent.TabIndex = 0;
+            this.lblStudent.Text = "Student: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gender";
+            // 
+            // cboGender
+            // 
+            this.cboGender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboGender.FormattingEnabled = true;
+            this.cboGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cboGender.Location = new System.Drawing.Point(60, 42);
+            this.cboGender.Name = "cboGender";
+            this.cboGender.Size = new System.Drawing.Size(121, 21);
+            this.cboGender.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Communist",
+            "Socialist",
+            "Social",
+            "Centrist",
+            "Market",
+            "Capitalist",
+            "Laissez-Faire"});
+            this.comboBox1.Location = new System.Drawing.Point(60, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Cosmopolitan",
+            "Internationalist",
+            "Peaceful",
+            "Balanced",
+            "Patriotic",
+            "Nationalist",
+            "Chauvinist"});
+            this.comboBox2.Location = new System.Drawing.Point(60, 96);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Anarchist",
+            "Libertarian",
+            "Liberal",
+            "Moderate",
+            "Statist",
+            "Authoritarian",
+            "Totalitarian"});
+            this.comboBox3.Location = new System.Drawing.Point(60, 123);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Revolutionary",
+            "Very Progressive",
+            "Progressive",
+            "Neutral",
+            "Traditional",
+            "Very Traditional",
+            "Reactionary"});
+            this.comboBox4.Location = new System.Drawing.Point(60, 150);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 7;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Anarcho-Communism",
+            "Libertarian Communism",
+            "Trotskyism",
+            "Marxism",
+            "De Leonism",
+            "Leninism",
+            "Stalinism/Maoism",
+            "Religious Communism",
+            "State Socialism",
+            "Theocratic Socialism",
+            "Religious Socialism",
+            "Democratic Socialism",
+            "Revolutionary Socialism",
+            "Libertarian Socialism",
+            "Anarcho-Syndicalism",
+            "Left-Wing Populism",
+            "Theocratic Distributism",
+            "Distributism",
+            "Social Liberalism",
+            "Christian Democracy",
+            "Social Democracy",
+            "Progressivism",
+            "Anarcho-Mutualism",
+            "National Totalitarianism",
+            "Global Totalitarianism",
+            "Technocracy",
+            "Centrist",
+            "Liberalism",
+            "Religious Anarchism",
+            "Right-Wing Populism",
+            "Moderate Conservatism",
+            "Reactionary",
+            "Social Libertarianism",
+            "Libertarianism",
+            "Anarcho-Egoism",
+            "Nazism",
+            "Autocracy",
+            "Fascism",
+            "Capitalist Fascism",
+            "Conservatism",
+            "Neo-Liberalism",
+            "Classical Liberalism",
+            "Authoritarian Capitalism",
+            "State Capitalism",
+            "Neo-Conservatism",
+            "Fundamentalism",
+            "Libertarian Capitalism",
+            "Market Anarchism",
+            "Totalitarian Capitalism",
+            "Ultra-Capitalism",
+            "Anarcho-Capitalism"});
+            this.comboBox5.Location = new System.Drawing.Point(60, 177);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Eco";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Diplo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Civil";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Societal";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Overall";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cboGender);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStudent);
             this.Name = "frmMain";
             this.Text = "Data aggregation";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblStudent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboGender;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
