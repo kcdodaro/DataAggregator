e38220a444e9330fe6c4bff5233f0325e939a657
Kyle <kcdodaro@gmail.com>
2017-09-17 15:23:40 -0400

    Added class values

--- DataAggregation/Student.cs
+++ DataAggregation/Student.cs
@@ -10,10 +10,29 @@ namespace DataAggregation
     {
         //Demographics
         public string strGender { get; set; }
+        public string strEthnicity { get; set; }
+
+        //Classifiers
         public int intAge { get; set; }
         public int strEco { get; set; }
         public int strDiplo { get; set; }
         public int strCivil { get; set; }
         public int strSocietal { get; set; }
+
+        //Values
+        public int intEquality { get; set; }
+        public int intNation { get; set; }
+        public int intLiberty { get; set; }
+        public int intTradition { get; set; }
+        public int intMarkets { get; set; }
+        public int intWorld { get; set; }
+        public int intAuthority { get; set; }
+        public int intProgress { get; set; }
+
+        //Constructors
+        public Student()
+        {
+
+        }
     }
 }
